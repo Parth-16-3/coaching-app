b539303d49a7fcb4b320a6ddec054966ed9eda27
Atharv <atharvsingh19112011@gmail.com>
2026-09-09 15:40:40 +0530

    added react app code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Coaching Directory: Vidisha</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
+export default App;
